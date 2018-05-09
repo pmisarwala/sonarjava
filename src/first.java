20137c87994ea210309827f6bf65b657bab4fe68
parvez <parvezmisarwala@gmail.com>
2018-05-09 07:59:44 +0530

    initial commit

---
+++ src/first.java
@@ -0,0 +1,16 @@
+public class first{
+	public static void main (String[] args) {
+		System.out.println("adfafHello Sonar World...!!");
+	}
+	class A{
+    	private  String TEST = "";
+    	A(){}
+    	public int dummy(int number) {
+    		number = temp;
+    		if (number==10) {
+    			System.out.println("Ten");
+    		}
+    	}
+    	
+    }
+}
